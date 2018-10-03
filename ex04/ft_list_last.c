728275266b64186339e0a843ad410abb49346939
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 16:34:49 -0700

    ex04

---
+++ ex04/ft_list_last.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_last.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/03 16:32:26 by seli              #+#    #+#             */
+/*   Updated: 2018/10/03 16:34:39 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_last(t_list *begin_list)
+{
+	if (!begin_list)
+		return (begin_list);
+	while (begin_list->next)
+		begin_list = begin_list->next;
+	return (begin_list);
+}
