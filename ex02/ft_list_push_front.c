9add667261db2318af2684a40ca1318ac2cec8f4
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 16:24:51 -0700

    ex02

--- ex02/ft_list_push_front.c
+++ ex02/ft_list_push_front.c
@@ -6,11 +6,23 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/03 15:59:59 by seli              #+#    #+#             */
-/*   Updated: 2018/10/03 16:00:13 by seli             ###   ########.fr       */
+/*   Updated: 2018/10/03 16:24:47 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+
 void	ft_list_push_front(t_list **begin_list, void *data)
 {
+	t_list	*head;
 
+	head = malloc(sizeof(t_list));
+	if (head == 0)
+		return ;
+	head->data = data;
+
+	if (!begin_list)
+		head->next = 0;
+	else
+		head->next = *begin_list;
 }
