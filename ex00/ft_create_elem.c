e1f5d6013b02e1984c0f78f458d02a68a2a96326
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 15:42:41 -0700

    ex00

---
+++ ex00/ft_create_elem.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/03 15:39:14 by seli              #+#    #+#             */
+/*   Updated: 2018/10/03 15:42:34 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_list.h"
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list	*ptr;
+
+	ptr = malloc(sizeof(t_list));
+	if (ptr == NULL)
+		return (ptr);
+	ptr->data = data;
+	ptr->next = NULL;
+	return (ptr);
+}
