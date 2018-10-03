7de12763ee1109c93b292e8dcd70a20e3e49860c
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 16:29:07 -0700

    update begin_list

--- ex02/ft_list_push_front.c
+++ ex02/ft_list_push_front.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/03 15:59:59 by seli              #+#    #+#             */
-/*   Updated: 2018/10/03 16:24:47 by seli             ###   ########.fr       */
+/*   Updated: 2018/10/03 16:28:58 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	ft_list_push_front(t_list **begin_list, void *data)
 	if (head == 0)
 		return ;
 	head->data = data;
-
 	if (!begin_list)
 		head->next = 0;
 	else
 		head->next = *begin_list;
+	begin_list = &head;
 }
