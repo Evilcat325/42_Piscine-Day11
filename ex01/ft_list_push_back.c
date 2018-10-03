fe0281404734bd4affdecd2f311e6168e7999ceb
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 15:58:12 -0700

    ex01

--- ex01/ft_list_push_back.c
+++ ex01/ft_list_push_back.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/03 15:43:00 by seli              #+#    #+#             */
-/*   Updated: 2018/10/03 15:51:39 by seli             ###   ########.fr       */
+/*   Updated: 2018/10/03 15:58:06 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ void	ft_list_push_back(t_list **begin_list, void *data)
 	if (head == 0)
 		return ;
 	head->data = data;
-
+	head->next = *begin_list;
+	begin_list = &head;
 }
