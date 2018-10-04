06fdb52277f4f888f2220426358b506ed71f2b54
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 20:30:31 -0700

    ex05

--- ex05/ft_list_push_params.c
+++ ex05/ft_list_push_params.c
@@ -6,26 +6,37 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/03 16:35:07 by seli              #+#    #+#             */
-/*   Updated: 2018/10/03 16:40:01 by seli             ###   ########.fr       */
+/*   Updated: 2018/10/03 20:30:16 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list	*head;
+
+	head = ft_create_elem(data);
+	if (head == 0 || !begin_list)
+		return ;
+	if (!*begin_list)
+		head->next = 0;
+	else
+		head->next = *begin_list;
+	*begin_list = head;
+}
+
 t_list	*ft_list_push_params(int ac, char **av)
 {
 	int		i;
-	t_list	*head;
-	t_list	*push;
+	t_list	*list;
 
 	i = 0;
-	head = 0;
+	list = 0;
 	while (i < ac)
 	{
-		push = ft_create_elem(av[ac]);
-		push->next = head;
-		head = push;
+		ft_list_push_front(&list, av[i]);
 		i++;
 	}
-	return (head);
+	return (list);
 }
