5917ba873657a9fb759aa4abf0af3907f95b05c1
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 21:33:55 -0700

    foreach if

---
+++ ex10/ft_list_foreach_if.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach_if.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/03 21:30:08 by seli              #+#    #+#             */
+/*   Updated: 2018/10/03 21:33:51 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach_if(t_list *begin_list, void (*f)(void *),
+							void *data_ref, int (*cmp)(void *, void *))
+{
+	while (begin_list)
+	{
+		if (cmp(begin_list->data, data_ref) == 0)
+			f(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
