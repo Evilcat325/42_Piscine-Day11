cb24a31ff1a97e4885ca189d6cd5f455da85ae6a
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 20:04:02 -0700

    updated push front logic

--- ex02/ft_list_push_front.c
+++ ex02/ft_list_push_front.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/03 15:59:59 by seli              #+#    #+#             */
-/*   Updated: 2018/10/03 19:35:03 by seli             ###   ########.fr       */
+/*   Updated: 2018/10/03 20:03:38 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	ft_list_push_front(t_list **begin_list, void *data)
 	t_list	*head;
 
 	head = malloc(sizeof(t_list));
-	if (head == 0)
+	if (head == 0 || !begin_list)
 		return ;
 	head->data = data;
-	if (!begin_list)
+	if (!*begin_list)
 		head->next = 0;
 	else
 		head->next = *begin_list;
-	begin_list = &head;
+	*begin_list = head;
 }
