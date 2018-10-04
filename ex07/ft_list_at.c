f76163b9e81a40c858a1f7058147fbdfae23415e
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 21:13:44 -0700

    ex07

---
+++ ex07/ft_list_at.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_at.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/03 21:11:30 by seli              #+#    #+#             */
+/*   Updated: 2018/10/03 21:13:37 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_at(t_list *begin_list, unsigned int nbr)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < nbr && begin_list)
+	{
+		begin_list = begin_list->next;
+		i++;
+	}
+	return (begin_list);
+}
