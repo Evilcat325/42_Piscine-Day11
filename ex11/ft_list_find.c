2a0c82fdb1131db8f6bf94f122770199e30b024d
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-03 21:36:09 -0700

    list find

---
+++ ex11/ft_list_find.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_find.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/03 21:34:06 by seli              #+#    #+#             */
+/*   Updated: 2018/10/03 21:36:00 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
+{
+	while (begin_list)
+	{
+		if (cmp(begin_list->data, data_ref) == 0)
+			return (begin_list);
+		begin_list = begin_list->next;
+	}
+	return ((void *)0);
+}
